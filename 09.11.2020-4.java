f298507208713012a260f7e7961b5409ebc9e354
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-12-02 11:38:05 +0300

    Create 09.11.2020-4.java

---
+++ 09.11.2020-4.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class dfgdfg {
+    static int factorial(int x){
+        int result=1;
+        for (int i = 1; i <= x; i++)
+        {
+            result *= i;
+        }
+        return  result;
+    }
+
+}
